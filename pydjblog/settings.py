54b6d04806ffc08e774b79e667dc3fc38665d0f8
karnika122 <karnika.m96@gmail.com>
2020-01-27 03:26:15 -0500

    updated

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '(#3*n4!eifk-7!k_2zc7g%79gz-u%ml5z)gyjd)=hlyp95h667'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['calm-coast-65371.heroku.com']
 
 
 # Application definition
@@ -145,3 +146,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mishrakarnika11@gmail.com'
 EMAIL_HOST_PASSWORD ='12345karnika'
+
+
+django_heroku.settings(locals())
