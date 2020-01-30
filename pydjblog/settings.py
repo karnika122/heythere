8f792b99392f584719b55d43e336f79ad3db3437
karnika122 <karnika.m96@gmail.com>
2020-01-30 16:07:25 -0500

    updated

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -145,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'mishrakarnika11@gmail.com'
-# EMAIL_HOST_PASSWORD ='12345karnika'
+EMAIL_HOST_USER = 'mishrakarnika11@gmail.com'
+EMAIL_HOST_PASSWORD ='12345karnika'
 
 
 django_heroku.settings(locals())
