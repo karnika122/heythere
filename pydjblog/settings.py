f2f4be60b81b8df6d8e69a17043b963846f8b904
karnika122 <karnika.m96@gmail.com>
2020-02-03 23:45:31 -0500

    updated

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -149,4 +149,4 @@ EMAIL_HOST_USER = 'mishrakarnika11@gmail.com'
 EMAIL_HOST_PASSWORD ='12345karnika'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
